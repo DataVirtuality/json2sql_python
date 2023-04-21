af955394eb88117def78e8c1e7014842460cb96f
CarlosKlapp <43484163+CarlosKlapp@users.noreply.github.com>
2023-04-21 11:10:15 -0600

    Initial commit

---
+++ gen_csv_files.py
@@ -0,0 +1,40 @@
+from typing import Any
+import pandas as pd
+from flatten01 import json_to_dataframe
+from flatten02 import to_csv
+from flatten03 import flatten
+from flatten04 import flatten_nested_json_df
+import flatten_json
+
+
+def gen_csv_files(json_obj: Any, file_name: str) -> None:
+    df = pd.json_normalize(json_obj, sep='/')
+    df.to_csv(f'{file_name}.00.csv')
+
+    df = json_to_dataframe(json_obj)
+    df.to_csv(f'{file_name}.01.csv')
+
+    # broken
+    # if isinstance(json_obj, list):
+    #     dic_flattened = flatten({"root": json_obj})
+    #     to_csv(dic_flattened, filename=f'{file_name}.02.csv')
+    # else:
+    #     to_csv(json_obj, filename=f'{file_name}.02.csv')
+
+    if isinstance(json_obj, list):
+        dic_flattened = flatten({"root": json_obj})
+    else:
+        dic_flattened = flatten(json_obj)
+    df = pd.DataFrame(dic_flattened, index=[0])
+    df.to_csv(f'{file_name}.03.csv')
+
+    if isinstance(json_obj, list):
+        dic_flattened = flatten({"root": json_obj})
+    else:
+        dic_flattened = flatten_json.flatten(json_obj)
+    df = pd.DataFrame(dic_flattened, index=[0])
+    df.to_csv(f'{file_name}.04.csv')
+
+    df = flatten_nested_json_df(pd.json_normalize(json_obj, sep='/'))
+    df.to_csv(f'{file_name}.05.csv')
+    print()
---
+++ main.py
@@ -0,0 +1,175 @@
+from typing import Dict, List, Self, cast, Any
+import json
+import argparse
+import glob
+# import sys
+from dataclasses import dataclass
+import fileinput
+from enum import Enum
+import pandas as pd
+
+from gen_csv_files import gen_csv_files
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-sio", "--stdio", action="store_true",
+                    help="Read json from STDIN.")
+parser.add_argument("-f", "--files", default='*.json', required=False,
+                    help="Read json files. Supports wildcards. Case insensitive search.")
+parser.add_argument("-r", "--recurse", action="store_true", required=False,
+                    help="Recursively search subfolders for json files.")
+parser.add_argument("-ih", "--include_hidden", action="store_true",
+                    required=False, help="Include hidden json files.")
+
+args = parser.parse_args()
+
+
+class DataTypes(Enum):
+    UNKNOWN = 'unknown'
+    STR = 'string'
+    INT = 'integer'
+    FLOAT = 'float'
+    # ARRAY = 'array'
+
+
+@dataclass
+class ObjInfo:
+    parent_xpath: str
+    current_xpath: str
+    xpath: str
+    _data_type: DataTypes = DataTypes.UNKNOWN
+    cardinality: int = 1
+
+    def inc_cardinality(self) -> None:
+        self.cardinality += 1
+
+    @property
+    def data_type(self):
+        return self._data_type
+
+    @data_type.setter
+    def data_type(self, datatype: DataTypes):
+        if datatype != self._data_type:
+            if self._data_type is DataTypes.UNKNOWN:
+                self._data_type = datatype
+            elif datatype == DataTypes.UNKNOWN:
+                pass
+            elif datatype in (DataTypes.INT, DataTypes.FLOAT) and self._data_type in (DataTypes.INT, DataTypes.FLOAT):
+                self._data_type = DataTypes.FLOAT
+            elif datatype == DataTypes.STR and self._data_type in (DataTypes.INT, DataTypes.FLOAT):
+                self._data_type = DataTypes.STR
+
+
+DICT_OBJ_INFO = Dict[str, ObjInfo]
+
+
+@dataclass
+class HierarchyInfo:
+    current_element: str
+    xpath: str
+    children: List[Self]
+
+
+def get_data_type(obj: Any) -> DataTypes:
+    if obj is None:
+        return DataTypes.UNKNOWN
+    if isinstance(obj, str):
+        return DataTypes.STR
+    if isinstance(obj, float):
+        return DataTypes.FLOAT
+    if isinstance(obj, int):
+        return DataTypes.INT
+    # if isinstance(obj, list):
+    #     return DataTypes.ARRAY
+    raise Exception('Unhandled type')
+    return DataTypes.UNKNOWN
+
+
+def register_key_value(parsed_data: DICT_OBJ_INFO, parent_xpath: str, current_xpath: str, data_type: DataTypes) -> None:
+    xpath = f'{parent_xpath}{current_xpath}/'
+    if xpath not in parsed_data:
+        parsed_data[xpath] = ObjInfo(
+            parent_xpath=parent_xpath, current_xpath=current_xpath, xpath=xpath, _data_type=data_type)
+    else:
+        oi: ObjInfo = parsed_data[xpath]
+        oi.data_type = data_type
+        oi.inc_cardinality()
+
+
+def parse_struct(obj: Any, parent_xpath: str, current_xpath: str, parsed_data: DICT_OBJ_INFO) -> None:
+    if isinstance(obj, dict):
+        d = cast(dict, obj)
+        for key, val in d.items():
+            if isinstance(val, dict):
+                parse_struct(
+                    obj=val, parent_xpath=f'{parent_xpath}{key}/', current_xpath=f'{key}', parsed_data=parsed_data)
+            elif isinstance(val, list):
+                parse_struct(
+                    obj=val, parent_xpath=f'{parent_xpath}', current_xpath=f'{key}', parsed_data=parsed_data)
+            else:
+                register_key_value(parsed_data=parsed_data, parent_xpath=parent_xpath,
+                                   current_xpath=key, data_type=get_data_type(val))
+    elif isinstance(obj, list):
+        l = cast(list, obj)
+        for item in l:
+            if isinstance(item, dict):
+                parse_struct(obj=item, parent_xpath=f'{parent_xpath}{current_xpath}/',
+                             current_xpath=current_xpath, parsed_data=parsed_data)
+            elif isinstance(item, list):
+                parse_struct(obj=item, parent_xpath=f'{parent_xpath}{current_xpath}/',
+                             current_xpath=current_xpath, parsed_data=parsed_data)
+            else:
+                register_key_value(parsed_data=parsed_data, parent_xpath=parent_xpath,
+                                   current_xpath=current_xpath, data_type=get_data_type(item))
+    else:
+        register_key_value(parsed_data=parsed_data, parent_xpath=parent_xpath,
+                           current_xpath=current_xpath, data_type=get_data_type(obj))
+
+
+def helper_determine_children_by_path(parsed_data: DICT_OBJ_INFO) -> HierarchyInfo:
+    hi_root: HierarchyInfo = HierarchyInfo('root', '/root/', [])
+
+    for key, val in parsed_data.items():
+        xpath: str = '/root/'
+        hi_current = hi_root
+        elements = key.split('/')
+        elements = [x for x in elements if len(x) > 0]
+        for i in range(1, len(elements)):
+            item = elements[i]
+            l = [x for x in hi_current.children if x.current_element == item]
+            assert len(l) <= 1
+            xpath = f'{xpath}{item}/'
+            if len(l) == 0:
+                hi = HierarchyInfo(item, xpath, [])
+                hi_current.children.append(hi)
+                hi_current = hi
+            else:
+                hi_current = l[0]
+    return hi_root
+
+
+def parsed_data_2_sql(parsed_data: DICT_OBJ_INFO) -> str:
+    hi: HierarchyInfo = helper_determine_children_by_path(parsed_data)
+
+    return ''
+
+
+def generate_dv_sql(obj: Any) -> str:
+    parsed_data: DICT_OBJ_INFO = {}
+    parse_struct(obj=obj, parent_xpath='/root/',
+                 current_xpath='root', parsed_data=parsed_data)
+    return parsed_data_2_sql(parsed_data)
+
+
+if args.stdio is True:
+    json_obj = json.loads(fileinput.input())
+    print(generate_dv_sql(json_obj))
+else:
+    for file_name in glob.iglob(args.files, recursive=args.recurse, include_hidden=args.include_hidden):
+        with open(file_name, 'rt', encoding='UTF-8') as f:
+            json_obj = json.load(f)
+            sql = generate_dv_sql(json_obj)
+            with open(f'{file_name}.sql', 'wt', encoding='UTF-8') as fsql:
+                fsql.write(sql)
+            gen_csv_files(json_obj, file_name)
